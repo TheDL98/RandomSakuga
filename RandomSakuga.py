TheDL98/RandomSakuga

commit 3477d92d92865ea9e03ed8fccf34296347ef4ad6

Fix a bug were the time line is not overwritten in certain conditions in Linux terminal.

RandomSakuga.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Random Sakuga.  If not, see <http://www.gnu.org/licenses/>.
 
+from sys import stdout
 from time import strftime, localtime, sleep
 from tempfile import NamedTemporaryFile
 import requests
@@ -75,6 +76,7 @@ if single_mode:
 # Scheduler setup
 if schedule_mode:
     while True:
+        stdout.write('\033[2K\033[1G') # Erase and go to beginning of line
         print(strftime("%H:%M", localtime()), end="\r")
 
         # Scheduler

commit 72f6e1f2d75aacf4d0123e6ec027ecace16f8844

Fix a Windows specific issue introduced in commit 3477d92

RandomSakuga.py
@@ -20,10 +20,11 @@ from time import strftime, localtime, sleep
 from tempfile import NamedTemporaryFile
 import requests
 import logging
+import os
 import json
 import funcs
 
-version = "V1.15"
+version = "V1.15.1"
 print(f"RandomSakuga {version}", end="\n\n")
 
 
@@ -119,7 +120,8 @@ try:
     # Scheduler setup
     if schedule_mode:
         while True:
-            stdout.write("\033[2K\033[1G")  # Erase and go to beginning of line
+            if os.name == "posix":
+                stdout.write("\033[2K\033[1G")  # Erase and go to beginning of line
             print(strftime("%H:%M", localtime()), end="\r")
 
             # Scheduler