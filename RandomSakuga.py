1033d6864af703c932ce8bf410cd8caeacdfdfc5
TheDL98 <ahmedalkadhim20001@gmail.com>
2021-10-08 00:59:49 +0300

    Add stream logger

--- RandomSakuga.py
+++ RandomSakuga.py
@@ -24,17 +24,28 @@ import os
 import json
 import funcs
 
-version = "V1.15.1"
+version = "V1.15.2-dev"
 print(f"RandomSakuga {version}", end="\n\n")
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(levelname)s<%(asctime)s>%(module)s:%(message)s", "%Y-%m-%d %H:%M:%S")
+
+file_handler_format = logging.Formatter(
+    "%(levelname)s<%(asctime)s>%(module)s:%(message)s", "%Y-%m-%d %H:%M:%S"
+)
+strream_handler_format = logging.Formatter("%(levelname)s:%(module)s:%(message)s")
+
 file_handler = logging.FileHandler("RandomSakuga.log")
 file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(file_handler_format)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARNING)
+stream_handler.setFormatter(strream_handler_format)
+
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 # Load settings
